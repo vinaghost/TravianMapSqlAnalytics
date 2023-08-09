ea4dd77ee816e62d59d9ee9f736bc384bf82561a
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-08-09 10:57:08 +0700

    update database service

---
+++ MapSqlDatabaseUpdate/Extensions/VillageRawExtension.cs
@@ -0,0 +1,67 @@
+﻿using MainCore.DatabaseModels;
+using MapSqlDatabaseUpdate.Models;
+
+namespace MapSqlDatabaseUpdate.Extensions
+{
+    public static class VillageRawExtension
+    {
+        public static Alliance GetAlliace(this VillageRaw villageRaw)
+        {
+            return new Alliance
+            {
+                AllianceId = villageRaw.AllianceId,
+                Name = villageRaw.AllianceName
+            };
+        }
+
+        public static Player GetPlayer(this VillageRaw villageRaw)
+        {
+            return new Player
+            {
+                PlayerId = villageRaw.PlayerId,
+                Name = villageRaw.PlayerName,
+                AllianceId = villageRaw.AllianceId
+            };
+        }
+
+        public static Village GetVillage(this VillageRaw villageRaw)
+        {
+            return new Village
+            {
+                VillageId = villageRaw.VillageId,
+                MapId = villageRaw.MapId,
+                Name = villageRaw.VillageName,
+                Tribe = villageRaw.Tribe,
+                X = villageRaw.X,
+                Y = villageRaw.Y,
+                PlayerId = villageRaw.PlayerId,
+                IsCapital = villageRaw.IsCapital,
+                IsCity = villageRaw.IsCity,
+                IsHarbor = villageRaw.IsHarbor,
+                Population = villageRaw.Population,
+                Region = villageRaw.Region,
+                VictoryPoints = villageRaw.VictoryPoints
+            };
+        }
+
+        public static VillagePopulation GetVillagePopulation(this Village village, DateTime date)
+        {
+            return new VillagePopulation
+            {
+                VillageId = village.VillageId,
+                Population = village.Population,
+                Date = date,
+            };
+        }
+
+        public static PlayerAlliance GetPlayerAlliance(this Player player, DateTime date)
+        {
+            return new PlayerAlliance
+            {
+                PlayerId = player.PlayerId,
+                AllianceId = player.AllianceId,
+                Date = date,
+            };
+        }
+    }
+}
--- MapSqlDatabaseUpdate/Service/Implementations/UpdateDatabaseService.cs
+++ MapSqlDatabaseUpdate/Service/Implementations/UpdateDatabaseService.cs
@@ -1,4 +1,6 @@
 ﻿using MainCore;
+using MainCore.DatabaseModels;
+using MapSqlDatabaseUpdate.Extensions;
 using MapSqlDatabaseUpdate.Models;
 using MapSqlDatabaseUpdate.Service.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -14,10 +16,62 @@ namespace MapSqlDatabaseUpdate.Service.Implementations
             _contextFactory = contextFactory;
         }
 
-        public async Task Execute(List<VillageRaw> villages)
+        public async Task Execute(List<VillageRaw> villageRaws)
         {
             using var context = _contextFactory.CreateDbContext();
             await context.Database.EnsureCreatedAsync();
+
+            var alliances = GetAlliances(villageRaws);
+            await context.BulkSynchronizeAsync(alliances, options => options.SynchronizeKeepidentity = true);
+
+            var players = GetPlayers(villageRaws);
+            await context.BulkSynchronizeAsync(players, options => options.SynchronizeKeepidentity = true);
+
+            var villages = GetVillages(villageRaws);
+            await context.BulkSynchronizeAsync(villages, options => options.SynchronizeKeepidentity = true);
+
+            var date = DateTime.Today;
+            if (!context.VillagesPopulations.Any(x => x.Date == date))
+            {
+                var villagePopulations = GetVillagePopulations(villages, date);
+                await context.BulkInsertAsync(villagePopulations);
+            }
+
+            if (!context.PlayersAlliances.Any(x => x.Date == date))
+            {
+                var playerAlliances = GetPlayerAlliances(players, date);
+                await context.BulkInsertAsync(playerAlliances);
+            }
+        }
+
+        private static List<Alliance> GetAlliances(List<VillageRaw> villageRaws)
+        {
+            var alliances = villageRaws.DistinctBy(x => x.AllianceId).Select(x => x.GetAlliace()).ToList();
+            return alliances;
+        }
+
+        private static List<Player> GetPlayers(List<VillageRaw> villageRaws)
+        {
+            var players = villageRaws.DistinctBy(x => x.PlayerId).Select(x => x.GetPlayer()).ToList();
+            return players;
+        }
+
+        private static List<Village> GetVillages(List<VillageRaw> villageRaws)
+        {
+            var villages = villageRaws.Select(x => x.GetVillage()).ToList();
+            return villages;
+        }
+
+        private static List<VillagePopulation> GetVillagePopulations(List<Village> village, DateTime date)
+        {
+            var villagePopulations = village.Select(x => x.GetVillagePopulation(date)).ToList();
+            return villagePopulations;
+        }
+
+        private static List<PlayerAlliance> GetPlayerAlliances(List<Player> player, DateTime date)
+        {
+            var playerAlliance = player.Select(x => x.GetPlayerAlliance(date)).ToList();
+            return playerAlliance;
         }
     }
 }
