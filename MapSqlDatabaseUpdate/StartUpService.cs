c7cd820b877bf3fbf5b84cfdb88e392f35cea4ca
VINAGHOST <nguyentrungnhan87@gmail.com>
2023-12-01 10:59:26 +0700

    order

--- MapSqlDatabaseUpdate/StartUpService.cs
+++ MapSqlDatabaseUpdate/StartUpService.cs
@@ -54,7 +54,7 @@ namespace MapSqlDatabaseUpdate
 
             await _mediator.Send(new UpdateServerListCommand([.. dataOutput]), cancellationToken);
 
-            var data = dataOutput.ToList();
+            var data = dataOutput.OrderByDescending(x => x.PlayerCount).ToList();
             _logger.LogInformation("Server: {count}", data.Count);
             data.ForEach(x => _logger.LogInformation("{output}", x));
             _hostApplicationLifetime.StopApplication();
