2f1f14cf1bc1874027292c3702f54a3fd7b09b18
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-01-08 00:24:00 +0700

    PlayerContainsAllianceHistory

---
+++ WebMVC/Views/Players/EditorTemplates/PlayerContainsAllianceHistoryParameters.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebMVC.Views.Players.EditorTemplates
+{
+    public class PlayerContainsAllianceHistoryParametersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
