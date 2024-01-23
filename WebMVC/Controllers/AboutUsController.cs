3a00ba21ebf9bf40cca34aa551405bc1f6e84510
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-01-23 17:12:02 +0700

    about us

---
+++ WebMVC/Controllers/AboutUsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Disclaimer()
+        {
+            return View();
+        }
+    }
+}
