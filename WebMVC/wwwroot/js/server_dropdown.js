709ced118349f4e7242987c040a9a628a1cc573a
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-02-24 09:35:05 +0700

    add toast

--- WebMVC/wwwroot/js/server_dropdown.js
+++ WebMVC/wwwroot/js/server_dropdown.js
@@ -9,6 +9,17 @@ $(function () {
             });
         if (rawResponse.ok) {
             $("#currentServer").text(this.value);
+            Toastify({
+                text: "Your selected server is saved",
+                duration: 3000,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                //style: {
+                //    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                //},
+                onClick: function () { } // Callback after click
+            }).showToast();
         }
     });
 });
