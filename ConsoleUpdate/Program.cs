587f9e6023fe69942174a1d65fde08935c90cfc6
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-06-23 10:49:06 +0700

    fix https

--- ConsoleUpdate/Program.cs
+++ ConsoleUpdate/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleUpdate.Services;
+﻿using ConsoleUpdate.Commands;
+using ConsoleUpdate.Services;
 using Core.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,7 +15,18 @@ builder.Services.AddMediatR(cfg =>
 {
     cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+
+builder.Services
+    .AddHttpClient<GetServerListCommand>()
+    .ConfigurePrimaryHttpMessageHandler(builder =>
+         new HttpClientHandler
+         {
+             ServerCertificateCustomValidationCallback = (m, c, ch, e) => true
+         }
+    );
+
 builder.Services.AddHttpClient();
+
 builder.Services.AddHostedService<StartUpService>();
 
 var host = builder.Build();
